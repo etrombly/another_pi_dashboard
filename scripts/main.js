cf3a00470cb309e31caaa8afa808a7a0a9fdbfec
etrombly <etrombly@yahoo.com>
2016-09-25 04:32:57 +0200

    added backgrounds and resizing

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,23 @@
+$(window).resize(reflow);
+
+function reflow(){
+    if( $(window).width() / $(window).height() > 1){
+        $('#calendar').css('position', 'absolute');
+        $('#calendar').css('top', $('#container').css('top'));
+        $('#container').css('width', '49.5%');
+        $('#calendar').css('height', '99%');
+        $('#calendar').css('left', $(window).width() / 2);
+    } else {
+        $('#calendar').css('position', 'relative');
+        $('#calendar').css('top', 0);
+        $('#container').css('width', '99%');
+        $('#calendar').css('height', '60%');
+        $('#calendar').css('left', 0);
+    }
+}
+
 function init() {
+    reflow();
     startTime();
     checkWeather();
     checkCalendar();
@@ -6,83 +25,105 @@ function init() {
 }
 
 function checkWeather() {
-    getJSON("http://api.openweathermap.org/data/2.5/weather?q=wiesbaden&APPID=" + config.openweathermap_api + "&units=imperial",
-      function(err, data) {
-        if (err != null) {
-          document.getElementById('weather').innerHTML = "Couldn't retrieve weather data";
-        } else {
-          document.getElementById('weather').innerHTML = 'WEATHER NOW <i class="owf owf-{0}"></i> {1}F'.format(
-                                                              data.weather[0].id, Math.round(data.main.temp));
-          var sunrise = new Date(data.sys.sunrise * 1000);
-          var sunset = new Date(data.sys.sunset * 1000);
-          document.getElementById('sun').innerHTML = "SUNRISE {0} SUNSET {1}".format(
-                                                          getTime(sunrise), getTime(sunset));
-        }
+    $.ajax({url: "http://api.openweathermap.org/data/2.5/weather?q=wiesbaden&APPID=" + config.openweathermap_api + "&units=imperial",
+           type: "GET",
+           dataType: "json"})
+    .done(function(json) {
+        $('#weather').html('WEATHER NOW <i class="owf owf-{0}"></i> {1}F'.format(
+                                           json.weather[0].id, Math.round(json.main.temp)));
+        var sunrise = new Date(json.sys.sunrise * 1000);
+        var sunset = new Date(json.sys.sunset * 1000);
+        $('#sun').html("SUNRISE {0} SUNSET {1}".format(getTime(sunrise), getTime(sunset)));
+        updateBackground(json.weather[0].id.toString());})
+    .fail(function( xhr, status, errorThrown ) {
+        $('#weather').html("Couldn't retrieve weather data");
     });
-    getJSON("http://api.openweathermap.org/data/2.5/forecast?q=wiesbaden&APPID=" + config.openweathermap_api + "&units=imperial",
-      function(err, data) {
-        if (err == null) {
-          var sorted = {};
-          for (var i = 0; i < data.list.length; i++){
-              var d = new Date(data.list[i].dt * 1000);
-              if (typeof sorted[d.getDayOfWeek()] == "undefined") {
-                  sorted[d.getDayOfWeek()] = [];
-              }
-              sorted[d.getDayOfWeek()].push(data.list[i]);
-          }
-          var rows = [];
-          var table = document.getElementById("forecast_table");
-          table.innerHTML = "";
-          for (var i = 0; i < 9; i++){
-              rows.push(table.insertRow(i));
-          }
-          rows[0].insertCell(0);
-          var index = 1;
-          for (var i = 1; i < 9; i++){
-              var d = new Date(sorted[Object.keys(sorted)[Object.keys(sorted).length - 2]][i-1].dt * 1000);
-              var cell = rows[i].insertCell(0);
-              cell.innerHTML = getTime(d);
-          }
-          for (key in sorted){
-              var cell = rows[0].insertCell(index);
-              cell.innerHTML = key;
-              var cells = [];
-              for (var i = 1; i < 9; i++){
-                  cells.push(rows[i].insertCell(index));
-              }
-              for (var i = 0; i < 8; i++){
-                   if (typeof sorted[key][i] != "undefined"){
-                       for (var ii = 1; ii < 9; ii++){
-                           var d = new Date(sorted[key][i].dt * 1000);
-                           if (rows[ii].cells[0].innerHTML == getTime(d)){
-                               rows[ii].cells[index].innerHTML = '<i class="owf owf-{0}"></i> {1}F'.format(sorted[key][i].weather[0].id, Math.round(sorted[key][i].main.temp));
-                           }
-                       }
-                   }
-              }
-              index++;
-          }
+
+    $.ajax({url: "http://api.openweathermap.org/data/2.5/forecast?q=wiesbaden&APPID=" + config.openweathermap_api + "&units=imperial",
+            type: "GET",
+            dataType: "jsonp"})
+    .done(function(json) {
+        var sorted = {};
+        for (var i = 0; i < json.list.length; i++){
+            var d = new Date(json.list[i].dt * 1000);
+            if (typeof sorted[d.getDayOfWeek()] == "undefined") {
+                sorted[d.getDayOfWeek()] = [];
+            }
+            sorted[d.getDayOfWeek()].push(json.list[i]);
+        }
+        var rows = [];
+        var table = document.getElementById('forecast_table');
+        table.innerHTML = "";
+        for (var i = 0; i < 9; i++){
+            rows.push(table.insertRow(i));
+        }
+        rows[0].insertCell(0);
+        var index = 1;
+        for (var i = 1; i < 9; i++){
+            var d = new Date(sorted[Object.keys(sorted)[Object.keys(sorted).length - 2]][i-1].dt * 1000);
+            var cell = rows[i].insertCell(0);
+            cell.innerHTML = getTime(d);
+        }
+        for (key in sorted){
+            var cell = rows[0].insertCell(index);
+            cell.innerHTML = key;
+            var cells = [];
+            for (var i = 1; i < 9; i++){
+                cells.push(rows[i].insertCell(index));
+            }
+            for (var i = 0; i < 8; i++){
+                if (typeof sorted[key][i] != "undefined"){
+                    for (var ii = 1; ii < 9; ii++){
+                        var d = new Date(sorted[key][i].dt * 1000);
+                        if (rows[ii].cells[0].innerHTML == getTime(d)){
+                            rows[ii].cells[index].innerHTML = '<i class="owf owf-{0}"></i> {1}F'.format(sorted[key][i].weather[0].id, Math.round(sorted[key][i].main.temp));
+                        }
+                    }
+                }
+            }
+            index++;
         }
     });
     var weatherTimer = setTimeout(checkWeather, 600000);
 }
 
+function updateBackground (weatherId) {
+    var sunny = ["800", "951"];
+    var thunder = ["200", "201", "202", "210", "211", "212", "221", "230", "231", "232"];
+    var drizzle = ["300", "301", "302", "310", "311", "312", "313", "314", "321"];
+    var rain = ["500", "501", "502", "503", "504", "511", "520", "521", "522", "531"];
+    var snow = ["600", "601", "602", "611", "612", "615", "616", "620", "621", "622"];
+    var cloud = ["801", "802", "803", "804"];
+    if (sunny.indexOf(weatherId) >= 0) {
+        $('body').css('background-image', 'url("sunny.jpg")');
+    } else if (thunder.indexOf(weatherId) >= 0) {
+        $('body').css('background-image', 'url("thunder.jpg")');
+    } else if (drizzle.indexOf(weatherId) >= 0) {
+        $('body').css('background-image', 'url("drizzle.jpg")');
+    } else if (rain.indexOf(weatherId) >= 0) {
+        $('body').css('background-image', 'url("rain.jpg")');
+    } else if (snow.indexOf(weatherId) >= 0) {
+        $('body').css('background-image', 'url("snow.jpg")');
+    } else if (cloud.indexOf(weatherId) >= 0) {
+        $('body').css('background-image', 'url("cloud.jpg")');
+    }
+}
+
 function getQuote(){
-    getJSON("http://quotes.rest/qod.json?category=inspire",
-      function(err, data) {
-        if (err != null) {
-          alert("Something went wrong: " + err);
-        } else {
-          var quote = data.contents.quotes[0];
-          document.getElementById('quote').innerHTML = '{0}<div style="text-align:right">-{1}</div>'.format(
-                                                              quote.quote, quote.author);
-        }
-    });
+    $.ajax({url: "http://quotes.rest/qod.json?category=inspire",
+          type: "GET",
+          dataType: "json"})
+    .done(function(json) {
+        var quote = json.contents.quotes[0];
+        $('#quote').html('{0}<div style="text-align:right">-{1}</div>'.format(
+                                                              quote.quote, quote.author));})
+    .fail(function( xhr, status, errorThrown ) {
+        $('#quote').html('Could not retrieve quote');});
     var quoteTimer = setTimeout(getQuote, 43200000);
 }
 
 function checkCalendar() {
-    document.getElementById('calendar').innerHTML = config.calendar_iframe;
+    $('#calendar').html(config.calendar_iframe);
     var calendarTimer = setTimeout(checkCalendar, 600000);
 }
 
@@ -93,7 +134,7 @@ function startTime() {
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('clock').innerHTML = '{0} {1} {2}<br>{3}:{4}:{5}'.format(today.getDayOfWeek(), today.getDate(), today.getMonthName(), h, m, s);
+    $('#clock').html('{0} {1} {2}<br>{3}:{4}:{5}'.format(today.getDayOfWeek(), today.getDate(), today.getMonthName(), h, m, s));
     var timeTimer = setTimeout(startTime, 1000);
 }
 
@@ -107,21 +148,6 @@ function getTime(d){
     var m = checkTime(d.getMinutes());
     return h + ":" + m;
 }
-
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("get", url, true);
-    xhr.responseType = "json";
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status);
-      }
-    };
-    xhr.send();
-};
 
 if (!String.prototype.format) {
   String.prototype.format = function() {
